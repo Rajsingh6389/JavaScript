e5c3e280bdec19402cb2a8f7f0d81e1dd64c3499
Rajsingh6389 <rm2739159@gmail.com>
2025-04-16 21:39:18 +0530

    classes in java script

---
+++ script.js
@@ -0,0 +1,39 @@
+// class ClassName{
+//     constructor(prob1,prob2){
+//         this.prob1=prob1
+//         this.prob2=prob2
+//     }
+// }
+// let  obj=new ClassName("arg1","arg2")
+// console.log(obj.prob1);
+
+// class Dog{
+//     constructor(dogName,weight,color,breed){
+//         this.dogName=dogName
+//         this.weight=weight
+//     }
+// }
+// let dog=new Dog('dogname',20,"black","breed")
+// console.log(dog.dogName);
+
+//classes are blueprint or template which is used to initialize methods and attributes
+
+// class Person{
+//     constructor(firstName,lastName){
+//         this.firstName=firstName
+//         this.lastName=lastName
+//     }
+// }
+
+// let p=new Person("rohit","sharma")
+// console.log(p.firstName +  " " + p.lastName)
+
+// class Person{
+//     constructor(firstName,lastName="sharma"){
+//         this.firstName=firstName
+//         this.lastName=lastName
+//     }
+// }
+
+let p=new Person("rohit")
+console.log(p.firstName +  " " + p.lastName)
